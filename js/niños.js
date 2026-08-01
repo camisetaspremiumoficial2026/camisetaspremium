3c568faf57f8d6a30c16ee66dd6286654cf668e6
victoriajuarez244 <104036975+victoriajuarez244@users.noreply.github.com>
2026-07-31 23:33:51 -0300

    a

--- js/niños.js
+++ js/niños.js
@@ -1,5 +1,4 @@
-const API_URL = 'http://localhost:3000';
-
+const API_URL = 'https://camisetaspremium.onrender.com';
 document.addEventListener("DOMContentLoaded", () => {
     const sideMenu = document.getElementById("sideMenu");
     const overlay  = document.getElementById("menuOverlay");
